723ccb1e85555e7b1eff87bbbf655bdba6632f32
msdhoni6200 <msdhoni62001@gmail.com>
2023-04-13 21:50:55 +0530

    pehla react project banaya

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img
+          src="https://static.tnn.in/thumb/msid-99349469,width-1280,height-720,resizemode-75/99349469.jpg"
+          className="App-logo"
+          alt="logo"
+        />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          saurabh kumar
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
